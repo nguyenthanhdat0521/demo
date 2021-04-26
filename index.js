57bdf3f9650d5b9fe6548e87e5b8a13c3c9b73b9
nguyenthanhdat0521 <nguyenthanhdat0521@gmail.com>
2021-04-26 15:50:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 app.get("/",(req,res)=>{
     res.send("hello");
-})
+});
 const port = process.env.PORT || 3000 ;
 app.listen(port,()=>{
-
-})
+    console.log("server listening to port "+port);
+});
