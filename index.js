0c811c16d6bd6488f5ef3eb68eaf7e10c6f043ec
nguyenthanhdat0521 <nguyenthanhdat0521@gmail.com>
2021-04-28 21:43:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname + '/public/index.html'));
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
-const port = process.env.PORT || 3001 ;
-app.listen(port,()=>{
-    console.log("server listening to port "+port);
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log("server listening to port " + port);
 });
