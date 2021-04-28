351eb73b29b9e8df0c22bada28492482cbe48cb8
nguyenthanhdat0521 <nguyenthanhdat0521@gmail.com>
2021-04-28 21:42:59 +0700

    dd
    dd

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require('path');
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 app.get("/",(req,res)=>{
-    res.send("hello");
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 3001 ;
 app.listen(port,()=>{
     console.log("server listening to port "+port);
 });
